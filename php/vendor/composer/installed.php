7c9dbf842032a33b2e92f553bd317243c6723c36
Howard Rosita <howard@thecamel.net>
2021-07-28 16:25:01 +0800

    adding componets

---
+++ php/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'a2bbd79b622f5b36b68a642272e544db36134693',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a2bbd79b622f5b36b68a642272e544db36134693',
+    ),
+    'stripe/stripe-php' => 
+    array (
+      'pretty_version' => 'v7.91.0',
+      'version' => '7.91.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'bb7db27140858833ecc2a95de94763b5a120bc5d',
+    ),
+  ),
+);
